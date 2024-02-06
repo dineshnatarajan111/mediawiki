297dac96cc43d264adb21c562532b9dd9ff9940b
dinesh <46438845+dineshnatarajan111@users.noreply.github.com>
2024-02-06 06:06:00 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -16,42 +16,6 @@ variable "environment" {
   default = ""
 }
 
-variable "network_security_group_name_public" {
-  description = ""
-  type = string
-  default = ""
-}
-
-variable "network_security_group_name_private" {
-  description = ""
-  type = string
-  default = ""
-}
-
-variable "virtual_network_name" {
-  description = ""
-  type = string
-  default = ""
-}
-
-variable "subnet_ilb_name" {
-  description = ""
-  type = string
-  default = ""
-}
-
-variable "subnet_blue_name" {
-  description = ""
-  type = string
-  default = ""
-}
-
-variable "subnet_green_name" {
-  description = ""
-  type = string
-  default = ""
-}
-
 variable "aks_name" {
   description = ""
   type = string
@@ -62,32 +26,4 @@ variable "aks_vm_size" {
   description = ""
   type = string
   default = ""
-}
-
-variable "mssql_server_name" {
-  description = ""
-  type = string
-  default = ""
-}
-
-variable "mssql_server_username" {
-  description = ""
-  type = string
-  default = ""
-}
-
-variable "mssql_server_password" {
-  description = ""
-  type = string
-  default = ""
-}
-
-variable "mssql_db_data" {
-  description = ""
-  type = list(object({
-    name = string
-  }))
-  default = [ {
-    name = ""
-  } ]
 }
